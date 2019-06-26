890ee443564c92487b7b89ee27731f0673943b1b
Marcelo Pepis <marcelo.pepis@gmail.com>
2019-06-26 12:14:49 -0300

    Metodos de Cryptografia implementados.
    Implementados os metodos open_key, crypto e decrypto.

--- run.py
+++ run.py
@@ -1,8 +1,57 @@
 import eel
+from cryptography.fernet import Fernet
+
+def key_generator():
+    key = Fernet.generate_key()
+
+    file = open('key.key', 'wb')
+    file.write(key)
+    file.close()
+
+def open_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    print(key)
+    return(key)
 
 
-eel.init('web')
+@eel.expose
+def crypto(arquivo):
+    key = open_key()
+    with open (arquivo, 'rb') as file:
+        data = file.read()
+
+    fernet = Fernet(key)
+    encrypted = fernet.encrypt(data)
+
+    with open(arquivo + '.crypto', 'wb') as file_enc:
+        file_enc.write(encrypted)
+
+def decrypto(arquivo, key):
+    with open (arquivo, 'rb') as file:
+        data = file.read()
+    fernet = Fernet(key)
+    encrypted = fernet.decrypt(data)
+
+    arquivo = arquivo.replace('.crypto', '')
+    with open(arquivo, 'wb') as f:
+        f.write(encrypted)
+
+
+
+
+
+arquivo = 'extrato.jpg.crypto'
+key = open_key()
+#crypto(arquivo, key)
+decrypto(arquivo, key)
+
+
+
+
+'''eel.init('web')
 eel.start('index.html', block=False)
 
 while True:
-    eel.sleep(10)
+    eel.sleep(10)'''
